a93facc96c35557de80dfcc7409431f561d418f0
Sorrow <h2toan@gmail.com>
2021-03-10 17:26:36 +0700

    update dictionary logic

--- translate.js
+++ translate.js
@@ -1,14 +1,20 @@
 const DICTIONARY = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('DICTIONARY');
 const LAST_ROW = DICTIONARY.getLastRow();
-const KEY_MAP = DICTIONARY.getRange(`A2:B${LAST_ROW}`).getValues();
+const MAX_ROW = DICTIONARY.getMaxRows();
 
 function updateDictionary(ITEM_VARIATION) {
+
     const DATA = ITEM_VARIATION.flatMap(e => [
-        [e.variable1Name],
-        [e.variable1Value],
-        [e.variable2Name],
-        [e.variable2Value]
-    ]);
-    DICTIONARY.getRange(`A${LAST_ROW+1}:A${LAST_ROW+DATA.length}`).setValues(DATA)
-    DICTIONARY.getRange('A:B').removeDuplicates([1])
+        [e.variable1Name, 'Not defined yet!'],
+        [e.variable1Value, 'Not defined yet!'],
+        [e.variable2Name, 'Not defined yet!'],
+        [e.variable2Value, 'Not defined yet!']
+    ]).filter(e => isChingChong(e[0]));
+
+    if (LAST_ROW == MAX_ROW) {
+        DICTIONARY.insertRowsAfter(LAST_ROW, DATA.length);
+    }
+
+    DICTIONARY.getRange(`A${LAST_ROW+1}:B${LAST_ROW+DATA.length}`).setValues(DATA);
+    DICTIONARY.getRange('A:B').removeDuplicates([1]);
 }
