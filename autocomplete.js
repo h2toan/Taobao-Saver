580ab94f7936c65e31e2e3ccb806b5306959e522
Sorrow <h2toan@gmail.com>
2021-03-18 14:57:12 +0700

    update autocomplete

--- autocomplete.js
+++ autocomplete.js
@@ -20,14 +20,14 @@ export default function autocomplete(inp, arr) {
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
             /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (arr[i].label.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML = "<strong>" + arr[i].label.substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].label.substr(val.length);
                 /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.innerHTML += "<input type='hidden' value='" + arr[i].value + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function (e) {
                     /*insert the value for the autocomplete text field:*/
@@ -39,6 +39,9 @@ export default function autocomplete(inp, arr) {
                 a.appendChild(b);
             }
         }
+        if (a.hasChildNodes()) {
+            a.style.height = '255px';
+        };
     });
     /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function (e) {
